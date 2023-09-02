84e61e78d58c84e0294598f4f66aed2ed85fdac8
baiyao <18303620306@163.com>
2023-09-02 20:35:33 +0800

    DimAPP & DwdTrafficBaseLogSplit

---
+++ dim/DimApp.groovy
@@ -0,0 +1,345 @@
+﻿DimApp
+
+1. 本地环境准备
+StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironmnet()
+1.1 设置并行度
+env.setParallelism(4);
+
+2. 检查点设置
+2.1 开启检查点, 设置检查点时间是3000L, 设置检查点模式为精准一次
+env.enableCheckpointing(3000L, CheckpointMode.EXACTLY_ONCE);
+
+2.2 设置检查点超时时间为1min
+env.getCheckpointConfig().setCheckpointTimeout(60 * 1000L);
+
+2.3 设置两次检查点之间最小时间间隔
+env.getCheckpointConfig().setMinPauseBetweenCheckpoints(3000L);
+
+2.4 设置取消任务后是否保留外部检查点
+env.enableExternalizedcheckpoints(CheckpointConfig.ExternalizedCheckpointCleanup.RETAIN_ON_CANCELLATION);
+
+2.5 设置重启策略
+env.setRestartStrategy(RestartStrategies.failureRateRestart(3, Time.Days(1L), Time.Minutes(1L)));
+env.setRestartStrategy(RestartStrategies.failureRateRestart(3, Time.days(30), Time.seconds(3)));
+
+2.6 设置状态后端以及检查点保存路径
+env.setStateBackend(new HashMapStateBackend());
+env.getCheckpoint().setCheckpointStorage("hdfs://zoo1:8020/gmall/ck");
+env.getCheckpointConfig().setCheckpointStorage("hdfs://zoo1:8020/gmall/ck");
+
+2.7 设置hdfs的操作用户
+System.setProperty("HADOOP_USER_NAME", baiyao);
+
+3. 从kafka读取数据
+Properties props = new Properties();
+3.1 kafka集群地址
+props.setProperty("bootstrap.servers", "zoo1:9092,zoo2:9092,zoo3:9092");
+3.2 消费者组
+props.setProperty("group.id", "dim_app_group");
+
+topic = "topic_db";
+
+FlinkKafkaConsumer<String> flinkKafkaConsumer = new FlinkKafkaConsumer<String>(
+    // FlinkKafkaConsumer消费的主题
+    topic,
+    // 从kafka读取的数据需要反序列化
+    new KafkaDeserializationschema<String>() {
+        // 程序会一直运行, 永远没有最后一条数据, 返回false
+        isEndOfStream return false;
+        
+        deserialize(ConsumerRecord<byte[], byte[]> consumerRecord) {
+            // 由于new String()的参数列表中传入的参数不能为null, 否则会报错. 
+            // 所以, 需要判断一下consumerRecord.value是否为空, 为空的话就返回new String(consumerRecord.value()),
+            // 否则直接返回null
+            if (consumerRecord.value() != null)
+                return new String(consumerRecord.value());
+            else 
+                return null;
+        }
+
+        // 返回反序列化之后数据类型String
+        getProducedType()
+            return TypeInformation.of(String.class)
+    },
+    props
+)
+
+env
+    // 将FlinkKafkaConsumer<String>封装为流
+    .addSource(flinkKafkaConsumer)
+    // 静态导入语法, 将kafka读取过来的json字符串转化为JSONObject类型
+    .map(JSON::parseObject)
+    // 数据简单清洗: 能转化为jsonObj的数据向下传、过滤掉maxwell全量导入数据的时候开始和结束数据
+    .filter(
+        new FilterFunction<JSONObject>() {
+            Boolean filter(JSONObject jsonObj) {
+                try {
+                    // 如果"data"数据是完整json, 返回true, 也就是向下传
+                    jsonObj.getJSONObject("data");
+
+                    String type = jsonObj.getString("type");
+
+                    // 如果"data"是完整json, 同时还要注意maxwell初始bootstrap全量同步的时候有两条消息需要被过滤掉, 那么就返回false
+                    if ("bootstrap-start".equals(type) || "bootstrap-complete".equals(type)) {
+                        return false;
+                    } 
+
+                    return true;
+                } catch(Exception e) {
+                    // 不为完整字符串就返回false, 直接过滤掉
+                    return false;
+                }
+            }
+        }
+    );
+
+-> filterDataStream
+// 使用flinkCDC读取Mysql表中配置数据
+MySqlSource<String> mysqlSource = MySqlSource
+    .<String>Builder()
+    .hostname()
+    .port()
+    .databaseList()
+    .tableList()
+    .username()
+    .password()
+    .deserializer(new JsonDebeziumDeserializationSchema())
+    .startupOptions(StartOptions.initial())
+    .build();
+
+-> MysqlDataStreamSource
+
+// flinkCDC读取的配置数据, 读过来之后需要转换为实体类进行传递
+TableProcess {
+    String sourceTable;
+    String sinkTable;
+    String sinkColumns;
+    String sinkPK;
+    String sinkExtended;
+}
+
+// flinkCDC读取的配置数据需要被封装为流, 不指定水位线
+env.fromSource(
+    mysqlSource,
+    WaterMarkStrategy.noWatermarks(),
+    "MySqlSource"
+)
+
+// 配置数据需要被广播到每一个并行实例上, 用MapState存储配置数据, key为源表表名, value为实体类
+MysqlDataStreamSource.broadcast(
+    mapStateDescriptor = new MapStateDescriptor<String, TableProcess>() {
+        "mapStateDescriptor",
+        String.class,
+        TableProcess.class
+    }
+)
+
+-> broadcastStream
+
+// 主流和配置流需要通过connect算子进行合并
+filterDataStream.connect(
+    broadcastStream
+)
+.process(
+    BroadcastProcessFunction<JSONObject, String, JSONObject> {
+        // 在open生命周期开始的时候获取德鲁伊数据源, 这个主要是为了处理配置流的时候连接phoenix, 用与提前建表.
+        // 为的是防止维度表变更后, 配置表也变更了, 那么就需要在往phoenix表插入数据之前提前建好表
+        open() {
+            druidDataSource = DruidUtil.createDataSource();
+        }
+
+        // 对主流数据, 也就是从kafka读过来的数据进行处理
+        processElement<jsonObj jsonObj, ReadOnlyContext context, Collector<JSONObject> out> {
+            // 从算子状态MapState中拿变更数据对应的配置数据
+            TableProcess tableProcess = readOnlyContext.getBroadcastState(mapStateDescriptor).get(jsonObj.getString("table"));
+
+            // 能拿到说明是配置流数据
+            if (tableProcess != null) {
+                // 拿到变更数据
+                JSONObject dataJsonObj = jsonObj.getJOSNObject("data");
+
+                // 拿到配置数据的sinkColumns
+                String sinkColumns = tableProcess.getSinkColumns();
+
+                // 将sinkColumns转换为集合
+                String[] column = sinkColumns.split(",");
+                List<String> columnList = Arrays.asList(column);
+                
+                // 然后判断主流数据中某一列是否在配置表中有, 没有的话就从原来的jsonObj里面删除掉
+                Set<Map.Entry<String, Object>> entrySet = dataJsonObj.entrySet();
+                entrySet.removeIf(entry -> !columnList.contains(entry.getKey));
+
+                // 向下游传递数据之前先将type以及sink_table也放入datajson中供后面使用
+                dataJsonObj.put("type", jsonObj.getString("type"));
+                dataJsonObj.put("sink_table", tabeProcess.getSinkTable());
+
+                // 向下游传递data
+                out.collect(dataJsonObj);
+            }            
+        }
+
+        // 处理配置流数据
+        processBroadcastElement<String jsonStr, Context context, Collector<JSONObject> out> {
+            JSONObject jsonObj = jsonStr.parseObject("jsonStr");
+
+            // 需要拿到配置流中op操作, 因为d与cru两种操作类型不同, 对状态进行的操作也不一样
+            String operation = jsonObj.getString("op");
+
+            // 拿到算子状态, 为删除以及cru更改操作做准备
+            BroadcastState<String, TableProcess> broadcastState = context.getBroadcastState(mapStateDescriptor);
+
+            if (operation.equals("d")) {
+                // 删除的话, 通过将配置流中的before封装为实体类对象, 拿到被删除的表名, 使用remove将算子状态中的keyValue信息给删除了
+
+                TableProcess tableProcess = jsonObj.getObject("before", TableProcess);
+
+                broadcastState.remove(tableProcess.getSourceTable());
+
+            } else {
+                // c, r, u
+                // 如果是创建、读或者更新的话, 需要把状态中的数据拿到之后进行更新
+
+                // 将after封装为实体类, 为更新广播状态以及后面拼接建表语句做准备
+                TableProcess tableProcess = jsonObj.getObj("after", TableProcess);
+                // 将读取到的配置数据放入算子广播状态中
+                broadcastState.put(tableProcess.getSourceTable(), tableProcess);
+
+                // 需要通过实体类对象拿到目的表表名, 表字段, 表主键, 建表语句扩展来为后面拼接phoenixSql建表语句作准备
+                String sinkTable = tableProcess.getSinkTable();
+                String sinkColumns = tableProcess.getSinkColumns();
+                String sinkPrimaryKey = tableProcess.getSinkPrimaryKey();
+                String sinkExtended = tableProcess.getsinkExtended();
+
+                // 拼接建表语句
+                // 为防止建表扩展以及主键为空导致拼接建表语句报错, 提前赋好默认值
+                if (sinkExtended == null) sinkExtended = "";
+                if (sinkPrimaryKey == null) sinkPrimaryKey = "id";
+
+                // 使用StringBuilder进行建表语句的拼接, 因为拼接字段的时候需要用到循环, 相对于String更好一些
+                StringBuilder phoenixSql = new StringBuilder("create table if not exists " + SCHEMA + "." + sinkTable + " (")
+
+                String[] column = sinkColumns.split(",");
+
+                for(int i = 0, i < column.length, i++) {
+                    // 是主键就加primary key, 不是的话就不加
+                    if (column[i].equals(sinkPrimaryKey)) {
+                        phoenixSql.append(column[i] + " varchar primary key")
+                    } else {
+                        phoenixSql.append(column[i] + " varchar")
+                    }
+
+                    // 非最后一个字段的话就加上逗号
+                    if(i < column.length - 1) {
+                        phoenixSql.append(",")
+                    }
+                }
+
+                // 建表扩展不要忘了
+                phoenixSql.append(") " + sinkExtended);
+
+                // 使用德鲁伊数据源获取连接、获取操作对象、执行sql语句
+                try {
+                    Connection connection = druidDataSource.getConnection();
+                    PreparedStatement preparedStatement = connection.prepareStatement(phoenixSql.toString());
+
+                    preparedStatement.execute();
+                } catch(Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    // 最后在finally中别忘了得关闭connection和preparedstatement
+                    if (connection != null) {
+                        try {
+                            connection.close;
+                        } catch(Exception e) {
+                            e.printStackTrace;
+                        }
+                    }
+
+                    if (preparedStatement != null) {
+                        try {
+                            preparedStatement.close;
+                        } catch(Exception e) {
+                            e.printStackTrace;
+                        }
+                    }
+                }
+
+                // 将读取到的配置流数据放到状态中给存储起来: key为源表名, value为配置表中的一行数据信息
+                broadcastState.put(tableProcess.getSourceTable(), tableProcess);
+            }
+        }
+    }
+)
+// 向phoenix中写数据
+.addSink(
+    // 通过富函数在open方法中获取德鲁伊连接池
+    new RichSinkFunction<JSONObj>() {
+        private DruidDataSource druidDataSource;
+
+        open() {
+            druidDataSource = DruidUtil.createDataSource();
+        }
+
+        // 
+        invoke(JSONObject jsonObj, Context context) {
+            // 获取目的表名以及操作类型为拼接upsert语句以及后面删除redis缓存数据作准备
+            String sinkTable = jsonObj.getString("sink_table");
+            String type = jsonObj.getString("type");
+
+            jsonObj.remove("sink_table");
+            jsonObj.remove("type");
+
+            // 拼接建表语句, 通过upsert来保证精准一次
+            String phoenixSql = "upsert into " + SCHEMA + "." + sinkTable + " (" + StringUtils.join(jsonObj.keySet(), ",") + ") " + "values('" + StringUtils.join(jsonObj.values(), "','") + "')"
+
+            // 获取连接、获取操作对象、执行sql语句、释放connection以及preparedStatement资源
+            try {
+                Connnection connection = druidDataSource.getConnection;
+                PreparedStatement preparedStatement = connection.prepareStatement(phoenixSql);
+                preparedStatement.execute();
+            } catch() {
+                
+            } finally {
+                connection.close();
+                preparedStatement.close();
+            }
+        }
+    }
+)
+
+// 通过类名调用静态方法创建德鲁伊连接池, 使用懒汉式双重判断对象锁创建的德鲁伊连接池
+DruidUtil {
+    private static DruidDataSource druidDataSource;
+
+    static DruidDataSource createDataSource() {
+        
+        if (druidDataSource != null) {
+            Sychronized(DruidUtil.class) {
+                if (druidDataSource == null) {
+                    druidDataSource = new DruidDataSource();
+
+                    // 连接的驱动类名以及连接的路径
+                    druidDataSource.DriverClassName("org.apache.phoenix.jdbc.phoenixDriver");
+                    druidDataSource.Url("jdbc:phoenix:zoo1,zoo2,zoo3:2181");
+
+                    // 1. 初始化连接数量
+                    druidDataSource.setInitialSize(5);
+                    // 2. 最大连接数
+                    druidDataSource.setMaxActive(20);
+                    // 3. 空闲连接数
+                    druidDataSource.setMinIdle(5);
+                    // 4. 没有空闲连接时, 连接等待时长
+                    druidDataSource.setMaxWait(-1);
+                    // 5. 连接空闲多久进行回收
+                    // 6. 连接回收器多久回收一次
+                }
+            }
+        }
+
+        return druidDataSource;
+    }
+}
+
+
+4. 提交任务执行
+env.execute();
---
+++ dwd/DwdTrafficBaseLogSplit.groovy
@@ -0,0 +1,358 @@
+﻿DwdTrafficBaseLogSplit
+
+// 1. 本地环境准备
+// 指定流环境
+StreamExecutionEnvironment env = StreanmExecutionEnvironment.getExecutionEnvironment();
+// 设置并行度为4, 保证与kafka分区数一致
+env.setParallelism(4);
+
+// 2. 设置状态后端
+// 开启检查点、设置检查点时间为3秒以及检查点模式为精准一次
+env.enableCheckpointing(3000L, CheckpointingMode.EXACTLY_ONCE);
+// 设置检查点超时时间为一分钟
+env.getCheckpointConfig().setCheckpointTimeout(60 * 1000L);
+// 设置两次检查点之间的最小时间间隔为3秒
+env.getCheckpointConfig().setMinPauseBetweenCheckpoints(3000L);
+// 设置取消任务的时候是否保留外部检查点, 设置为保留
+env.getCheckpointConfig().enableExternalizedCheckpoints(ExternalizedCheckpointCleanup.RETAIN_ON_CANCELLATION);
+// 设置失败重启策略为故障率重启, 有效时间为1天, 每天重启机会为3次, 每两次间隔时间为1min
+env.setRestartStrategy(RestartStrategies.failureRateRestart(3, Time.days(1), Time.minutes(1)));
+// 设置状态后端
+// 设置状态在taskmanager内存中存储形式为HashMap
+env.setStateBackend(new HashMapStateBackend());
+// 设置远程检查点备份路径为hdfs
+env.getCheckpointConfig().setCheckpointStorage("hdfs://zoo1:8020/gmall/ck");
+// 设置hdfs操作用户为baiyao
+System.setProperty("HADOOP_USER_NAME", baiyao);
+
+// 3. 从kafka的topic_log读取日志数据
+Properties props = new Properties();
+props.setProperty("bootstrap.servers", "zoo1:9092");
+props.setProperty("group.id", "dwd_traffic_base_log_split_group");
+
+FlinkKafkaConsumer<String> flinkKafkaConsumer = new FlinkKafkaConsumer<String>() {
+    // 指定读取的topic
+    "topic_log",
+    // kafka反序列化方法, 返回值是String
+    new KafkaDeserializationSchema<String>() {
+        isEndOfStream return false;
+
+        deserialize(ConsumerRecord<byte [], byte[]> consumerRecord) {
+            if (consumerRecord.value() != null) {
+                return new String(consumerRecord.value());
+            }
+            return null;
+        }
+
+        getProducedType() return TypeInformation.of(String);
+    },
+    // kafka集群地址bootstrap.servers以及消费者组group.id
+    props
+}
+
+// 定义脏数据进入的侧输出流
+OutputTag<String> dirtyData = new OutputTag("dirty_data"){};
+// 定义错误日志侧输出流
+OutputTag<String> errorTag = new OutputTag("errorTag"){};
+// 定义启动日志侧输出流
+OutputTag<String> startTag = new OutputTag("startTag"){};
+// 定义曝光日志侧输出流
+OutputTag<String> displayTag = new OutputTag("displayTag"){};
+// 定义动作日志侧输出流
+OutputTag<String> actionTag = new OutputTag("actionTag"){};
+
+-> JsonObjDataStream
+
+env
+    // 将FlinkkakfaConsumer<String>封装为流
+    .addSource(flinkKafkaConsumer)
+    // 调process使用processFunction将数据转换为jsonObject格式, 不能转换的放入侧输出流中, 能转换的就转换成json字符串并往下游发送
+    .process(
+        new ProcessFunction<String, JSONObject>() {
+
+            processElement(String jsonStr, Context context, Collector<JSONObject> out) {
+                try {
+                    JSONObject jsonObj = JSON.parseObject(jsonStr);
+
+                    out.collect(jsonObj);
+                } catch(Exception e) {
+                    e.printStackTrace;
+
+                    context.output(dirty_data, jsonStr);
+                }
+            }
+        }
+    )
+
+Properties props = new Properties();
+props.setProperty("bootstrap.servers", "zoo1:9092");
+// 事务时间阈值一定要比检查点超时时间长来保证检查点做完
+props.setProperty("transaction.timeout.ms", 15 * 60 * 1000L);
+
+JsonObjDataStream
+    // 获取侧输出流
+    .getSideOutput(dirtyData)
+    // 将侧输出流数据发送至kafka主题中
+    .addSink(
+        new FlinkKafkaProducer<String>(
+            // 默认发送的topic
+            "default_topic",
+            // kafka序列化方法
+            new KafkaSerializationSchema<String>() {
+                ProducerRecord<byte[], byte[]> serialize(String str) {
+                    return new ProducerRecord<byte[], byte[]>("dirty_data", str.getBytes());
+                }
+            },
+            // 指定kafka集群地址bootstrap.servers以及事务的超时时间
+            props,
+            // 指定FlinkKafkaConsumer的语义为精准一次, 以此来保证开启事务机制, 至少一次语义是默认的, 他是不开启事务的
+            FlinkKafkaProducer.Semantic.EXACTLY_ONCE
+        )
+    );
+
+// 将主流中不能转换为完整json的数据放入侧输出流并存入kafka之后就需要将主流数据按照mid分组了
+JsonObjDataStream
+    // 将数据按照topic_log中common的mid分组, 确保后面拿到每一个状态的时候都是某个mid的上次访问日期
+    .keyBy(r -> r.getJSONObj("common").getString("mid"))
+    .map(
+        new RichMapFunction<JSONObject, JSONObject>() {
+            // 获取某一个mid的值状态
+            ValueState<String> lastVisitDateState;
+
+            // 值状态初始化
+            open() {
+                lastVisitDateState = getRuntimeContext().getState(new valueStateDesciptor<String>(
+                    "lastVisitDateState",
+                    String.class
+                ))
+            }
+
+/**
+        // 13位时间戳
+        // 1693638841043
+        System
+                .currentTimeMillis();
+        // 2023-09-02T07:14:01.044Z
+        new Date(
+                System
+                        .currentTimeMillis()
+        ).toInstant();
+        // 2023-09-02T15:14:01.143
+        LocalDateTime
+                .ofInstant(
+                        new Date(System.currentTimeMillis()).toInstant(), 
+                        ZoneId.systemDefault()
+                );
+        // 2023-09-02 15:14:01
+        DateTimeFormatter
+                .ofPattern("yyyy-MM-dd HH:mm:ss")
+                .format(
+                        LocalDateTime
+                                .ofInstant(new Date(System.currentTimeMillis()).toInstant(), 
+                                        ZoneId.systemDefault())
+                );
+*/
+
+            JSONObj map(JSONObj jsonObj) {
+                // 获取从kafka的topic_log中拿到的is_new新老访客标记
+                String is_new = jsonObj.getJSONObj("common").getString("is_new");
+                // 拿到上次访问日期
+                String lastVisitDate = lastVisitDateState.value();
+                // 拿到数据产生时携带的13位时间戳ts
+                Long ts = jsonObj.getLong("ts");
+
+                // 使用时间日期类将13位时间戳转换为2023-09-02的格式
+                String visitDate = DateTimeFormatter
+                    .ofPattern("yyyy-MM-dd")
+                    .format(
+                        LocalDateTime
+                            .ofInstant(
+                                new Date(ts).toInstant(),
+                                ZoneId.SystemDefault()
+                            )
+                    )
+
+                // 拿到新老访客标记is_new:
+                // 1: 如果上次访问日期为空, 是新访客, 将访问日期更新到状态中去; 如果上次访问日期不等于当前访问日期, 说明是今天的新访客, 更新状态
+                // 0: 如果是实时数仓刚建起来, 状态里面没有上次访问日期的话, 就需要将状态里面的上次访问日期更新为前一天
+
+                if ("1".equals(is_new)) {
+                    if (StringUtils.isEmpty(lastVisitDate)) {
+                        lastVisitDateState.update(visitDate);
+                    } else {
+                        if (!lastVisitDate.equals(visitDate)) {
+                            jsonObj.getJSONObject("commons").put("is_new", "0");
+                        }
+                    }
+                } else {
+                    if (StringUtils.isEmpty(lastVisitDate)) {
+                        lastVisitDateState.update(
+                            DateTimeFormatter
+                                .ofPattern("yyyy-MM-dd")
+                                .format(
+                                    LocalDateTime
+                                        .ofInstant(
+                                            new Date(ts - 24 * 60 * 60 * 1000L).toInstant(),
+                                            ZoneId.SystemDefault()
+                                        )
+                                )
+                        )
+                    }
+                }
+
+                return jsonObj;
+            }
+        }
+    )
+    .process(
+        new ProcessFunction<JSONObject, String>() {
+            processElement(JSONObject jsonObj, Context context, Collector<String> collector) {
+                // jsonObj中有"err", 就将数据发送至errorTag侧输出流, 最后remove掉"err"
+                // jsonObj中有"start", 就将数据发送至startTag侧输出流
+                // 从jsonObj中获取页面日志的三个属性: page、common、ts
+                //      将jsonObj中的displays切分, 新put("display", displayJSONObject), 得到曝光日志并放入displayTag中
+                //      将jsonObj中的actions切分, 新put("action", actionJSONObject), 得到动作日志并放入actionTag中
+
+                JSONObject errJsonObj = jsonObj.getJSONObject(err);    
+
+                if (errJsonObj != null) {
+                    context.output(errorTag, jsonObj.toJSONString());
+                    jsonObj.remove("err");
+                }
+
+                JSONObject startJsonObj = jsonObj.getJSONObject("start");
+
+                if (startJsonObj != null) {
+                    context.output(startTag, jsonObj.toJSONString());
+                } else {
+                    // 页面属性: page, common, ts
+
+                    JSONObject page = jsonObj.getJSONObject("page");
+                    JSONObject common = jsonObj.getJSONObject("common");
+                    Long ts = jsonObj.getLong("ts");
+                    JSONArray displays = jsonObj.getJSONArray("displays");
+                    JSONArray actions = jsonObj.getJSONArray("actions");
+
+                    // 删除曝光和动作信息, 将只包含page, common, ts的日志信息往下游发送
+                    jsonObj.remove("diplays");
+                    jsonObj.remove("actions");
+
+                    // 将曝光信息发送至曝光侧输出流displayTag
+                    if (displays != null && displays.size() > 0) {
+                        for (int i = 0; i < displays.size(); i++) {
+                            JSONObject displayJsonObj = new JSONObject()
+                            displayJsonObj.put("page", page);
+                            displayJsonObj.put("common", common);
+                            displayJsonObj.put("ts", ts);
+                            displayJsonObj.put("display", diplays.getJSONObject(i));
+                            context.output(displayTag, displayJsonObj.toJSONString());
+                        }
+                    }
+
+                    // 将动作信息发送至动作侧输出流actionTag
+                    if (actions != null && actions.size() > 0) {
+                        for (int i = 0; i < actions.size(); i++) {
+                            JSONObject actionJsonObj = new JSONObject();
+                            actionJsonObj.put("page", page);
+                            actionJsonObj.put("common", common);
+                            actionJsonObj.put("action", actions.getJSONObject(i));
+
+                            collector.collect(actionTag, actionJsonObj.toJSONString());
+                        }
+                    }
+
+                    collector.collect(jsonObj.toJSONString());
+                }
+
+
+            }
+        }
+    )
+
+    -> pageDataStream
+
+    Properties props = new Properties();
+    props.setProperty("bootstrap.servers", "zoo1:9092");
+    props.setProperty("transaction.timeout.ms", 15 * 60 * 1000L);
+
+    // 错误侧输出流数据写入kafka中
+    pageDataStream
+        .getSideOutput(errorTag)
+        .addSink(
+            new FlinkKafkaProducer<String> (
+                "default_topic",
+                new KafKaSerializationSchema<String>() {
+                    ProducerRecord<byte[], byte[]> serialize(String str) {
+                        return new ProducerRecord<byte[], byte[]>("dwd_traffic_err_log", str.getBytes());
+                    }
+                },
+                props,
+                FlinkKafkaProducer.Semantic.EXACTLY_ONCE
+            )
+        );
+
+    // 启动侧输出流数据写入kafka中
+    pageDataStream
+        .getSideOutput(startTag)
+        .addSink(
+            new FlinkKafkaProducer<String> (
+                "default_topic",
+                new KafKaSerializationSchema<String>() {
+                    ProducerRecord<byte[], byte[]> serialize(String str) {
+                        return new ProducerRecord<byte[], byte[]>("dwd_traffic_start_log", str.getBytes());
+                    }
+                },
+                props,
+                FlinkKafkaProducer.Semantic.EXACTLY_ONCE
+            )
+        );
+
+    // 曝光侧输出流数据写入kafka中
+    pageDataStream
+        .getSideOutput(displayTag)
+        .addSink(
+            new FlinkKafkaProducer<String> (
+                "default_topic",
+                new KafKaSerializationSchema<String>() {
+                    ProducerRecord<byte[], byte[]> serialize(String str) {
+                        return new ProducerRecord<byte[], byte[]>("dwd_traffic_display_log", str.getBytes());
+                    }
+                },
+                props,
+                FlinkKafkaProducer.Semantic.EXACTLY_ONCE
+            )
+        );
+
+    // 动作侧输出流数据写入kafka中
+    pageDataStream
+        .getSideOutput(actionTag)
+        .addSink(
+            new FlinkKafkaProducer<String> (
+                "default_topic",
+                new KafKaSerializationSchema<String>() {
+                    ProducerRecord<byte[], byte[]> serialize(String str) {
+                        return new ProducerRecord<byte[], byte[]>("dwd_traffic_action_log", str.getBytes());
+                    }
+                },
+                props,
+                FlinkKafkaProducer.Semantic.EXACTLY_ONCE
+            )
+        );
+
+    // 主流页面日志写入kafka中
+    pageDataStream
+        .addSink(
+            new FlinkKafkaProducer<String> (
+                "default_topic",
+                new KafKaSerializationSchema<String>() {
+                    ProducerRecord<byte[], byte[]> serialize(String str) {
+                        return new ProducerRecord<byte[], byte[]>("dwd_traffic_page_log", str.getBytes());
+                    }
+                },
+                props,
+                FlinkKafkaProducer.Semantic.EXACTLY_ONCE
+            )
+        );
+
+// 将有向无环图提交到集群上去
+env.execute();
